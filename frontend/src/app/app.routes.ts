dcced77828218d3672816c124b89d9ed882797b0
Evans Djangbah <djangbahevans@yahoo.com>
2024-07-23 11:56:57 +0000

    feat: Add signup component and routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
+import { SignupComponent } from './features/signup/signup.component';
 
 export const routes: Routes = [
   {
@@ -11,4 +12,8 @@ export const routes: Routes = [
     path: 'login',
     component: LoginComponent,
   },
+  {
+    path: "signup",
+    component: SignupComponent,
+  }
 ];
---
+++ frontend/src/app/features/signup/signup.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterLink],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css',
+})
+export class SignupComponent {
+  loginForm = new FormGroup({
+    full_name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  handleSubmit() {
+    console.log('handleSubmit() called');
+    console.log(this.loginForm.value);
+  }
+}
