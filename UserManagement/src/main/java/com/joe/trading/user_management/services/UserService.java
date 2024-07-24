ed11521e218de857baf61935e6ef8bd39614df44
Josiah Kotey <josiah.kotey@turntabl.io>
2024-07-24 17:27:57 +0000

    get all users endpoint

--- UserManagement/src/main/java/com/joe/trading/user_management/services/UserService.java
+++ UserManagement/src/main/java/com/joe/trading/user_management/services/UserService.java
@@ -1,5 +1,7 @@
 package com.joe.trading.user_management.services;
 
+import java.util.List;
+
 import com.joe.trading.user_management.dtos.CreateUserRequestDto;
 import com.joe.trading.user_management.entities.User;
 import com.joe.trading.user_management.exception.ResourceNotFoundException;
@@ -7,5 +9,5 @@ import com.joe.trading.user_management.exception.ResourceNotFoundException;
 public interface UserService {
     User createUser(CreateUserRequestDto createUserDto);
     User getUserById(Long userId) throws ResourceNotFoundException;
-
+    List<User> getAllUsers();
 }
