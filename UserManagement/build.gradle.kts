5053b0679b3b062f6ec68c8d096b09feff7a1ee8
Evans Djangbah <djangbahevans@yahoo.com>
2024-07-27 16:35:41 +0000

    chore: Add MapStruct library for object mapping

--- UserManagement/build.gradle.kts
+++ UserManagement/build.gradle.kts
@@ -31,6 +31,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 	compileOnly("org.projectlombok:lombok")
