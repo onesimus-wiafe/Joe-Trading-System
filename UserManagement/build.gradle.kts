704153db86c3df3075fc0c554ccbcbcd088a5d57
Evans Djangbah <djangbahevans@yahoo.com>
2024-07-29 15:16:09 +0000

    Update dependencies and remove unused project reference

--- UserManagement/build.gradle.kts
+++ UserManagement/build.gradle.kts
@@ -33,7 +33,6 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
 	implementation("org.mapstruct:mapstruct:1.5.5.Final")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
-	implementation(project(":shared"))
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 	compileOnly("org.projectlombok:lombok")
