750c4f9146e2c1dce9518a9bbbde898334804bc8
Evans Djangbah <djangbahevans@yahoo.com>
2024-07-31 21:11:27 +0000

    chore: Update scanBasePackages in UserManagementApplication

--- UserManagement/src/main/java/com/joe/trading/user_management/UserManagementApplication.java
+++ UserManagement/src/main/java/com/joe/trading/user_management/UserManagementApplication.java
@@ -3,7 +3,7 @@ package com.joe.trading.user_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.joe.trading")
 public class UserManagementApplication {
 
 	public static void main(String[] args) {
