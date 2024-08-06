bf5292d9a2bfdff14cb839f15a2b3d03f214ea01
Evans Djangbah <djangbahevans@yahoo.com>
2024-08-06 10:55:31 +0000

    chore: Add CORS support on API Gateway

--- GatewayService/Program.cs
+++ GatewayService/Program.cs
@@ -7,16 +7,23 @@ builder.Services.AddReverseProxy()
   .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 builder.Services.AddOpenTelemetry()
   .ConfigureResource(resource => resource.AddService(serviceName: builder.Environment.ApplicationName))
-  .WithMetrics(metrics => {
+  .WithMetrics(metrics =>
+  {
     metrics
       .AddAspNetCoreInstrumentation()
       .AddRuntimeInstrumentation()
       .AddPrometheusExporter();
   });
 
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy("AllowAll", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
 var app = builder.Build();
 
 app.UseOpenTelemetryPrometheusScrapingEndpoint();
 app.MapReverseProxy();
+app.UseCors("AllowAll");
 
 app.Run();
