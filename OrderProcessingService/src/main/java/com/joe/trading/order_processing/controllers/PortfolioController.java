d36628995bc3caec97f4dfcca5960f7051577cbf
Onesimus Wiafe <onesimus.wiafe@turntabl.io>
2024-08-07 19:52:03 +0000

    update controller

--- OrderProcessingService/src/main/java/com/joe/trading/order_processing/controllers/PortfolioController.java
+++ OrderProcessingService/src/main/java/com/joe/trading/order_processing/controllers/PortfolioController.java
@@ -1,15 +1,13 @@
 package com.joe.trading.order_processing.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.joe.trading.order_processing.entities.dto.PortfolioRequestDTO;
 import com.joe.trading.order_processing.entities.dto.PortfolioResponseDTO;
 import com.joe.trading.order_processing.services.PortfolioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/portfolio")
@@ -23,7 +21,7 @@ public class PortfolioController {
     }
 
     @PostMapping
-    public ResponseEntity<PortfolioResponseDTO> createPortfolio(PortfolioRequestDTO request){
+    public ResponseEntity<PortfolioResponseDTO> createPortfolio(@RequestBody PortfolioRequestDTO request) throws JsonProcessingException {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(portfolioService.saveNewPortfolio(request.getUserId(), request.getPortfolioName()));
     }
