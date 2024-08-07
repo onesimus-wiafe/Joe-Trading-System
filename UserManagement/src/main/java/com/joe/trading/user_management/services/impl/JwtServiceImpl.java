437d5a0c726e2c6a57d33de7f1a4f76b267aac05
Evans Djangbah <djangbahevans@yahoo.com>
2024-08-07 08:07:22 +0000

    chore: Added JWT authentication/authorization to the Gateway Service

--- UserManagement/src/main/java/com/joe/trading/user_management/services/impl/JwtServiceImpl.java
+++ UserManagement/src/main/java/com/joe/trading/user_management/services/impl/JwtServiceImpl.java
@@ -1,5 +1,8 @@
 package com.joe.trading.user_management.services.impl;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -32,6 +35,12 @@ public class JwtServiceImpl implements JwtService {
     @Value("${security.jwt.expiration-time}")
     private long jwtExpiration;
 
+    @Value("${security.jwt.audience}")
+    private String audience;
+
+    @Value("${security.jwt.issuer}")
+    private String issuer;
+
     public String extractEmail(String token) {
         return extractClaim(token, Claims::getSubject);
     }
@@ -59,11 +68,18 @@ public class JwtServiceImpl implements JwtService {
             long expiration) {
         return Jwts
                 .builder()
+                .header()
+                .keyId(generateKeyId())
+                .and()
                 .claims(extraClaims)
                 .subject(userDetails.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(getSignInKey())
+                .audience()
+                .add(audience)
+                .and()
+                .issuer(issuer)
                 .compact();
     }
 
@@ -92,5 +108,15 @@ public class JwtServiceImpl implements JwtService {
     private SecretKey getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private String generateKeyId() {
+        var key = getSignInKey();
+        try {
+            return Base64.getUrlEncoder().withoutPadding().encodeToString(
+                    MessageDigest.getInstance("SHA-256").digest(key.getEncoded()));
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
     }
 }
